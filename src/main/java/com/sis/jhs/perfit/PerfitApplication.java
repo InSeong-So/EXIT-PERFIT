b8169d6c37296172fec4c29e3a31bb3e49cfee77
InSeong-So <goflvhxj2547@gmail.com>
2019-03-09 17:06:14 +0900

    최초 생성

---
+++ src/main/java/com/sis/jhs/perfit/PerfitApplication.java
@@ -0,0 +1,13 @@
+package com.sis.jhs.perfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sis/jhs/perfit/PerfitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sis.jhs.perfit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PerfitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
